51026836f7cf883425e538945b5968fd2763a86e
sol0veyy <solovevdaniil03@mail.ru>
2024-01-21 17:05:26 +0300

    add navigation

--- src/app/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div>
-            <h1>Profile Page</h1>
-        </div>
-    );
-}
-
---
+++ src/app/registration/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Registration page</h1>
+        </div>
+    )
+}
