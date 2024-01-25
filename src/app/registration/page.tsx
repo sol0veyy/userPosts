6212e781be1705600981065389c7c2ed5ca65126
Danulla <straticiuk0208@gmail.com>
2024-01-25 19:31:00 +0200

    update

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -1,25 +1,40 @@
+import { FORMERR } from "dns"
 import "./style.css"
 
 export default function Page() {
     return (
-        <div className="d-flex justify-content-center">
+            
+       <div className="d-flex justify-content-center">
             <form className="w-25 registration__block">
+                
+                
+                <h2  className="text-center mb-3">Registration</h2> 
+                
+              
                 <div className="mb-3">
+
+                  <label htmlFor="exampleInputPassword1" className="form-label">Login</label>
+                    <input type="login" className="form-control" />
+                </div>
+                
+                
+                  <div className="mb-3">
                     <label htmlFor="exampleInputEmail1"  className="form-label">Email address</label> 
                     <input type="email"  className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/> 
-                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
+                
+                
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1"/>
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label> 
+                <div className="mb-1 form-check">
+                        <label className="form-check-label" > </label>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit"  className="btn btn-primary">Sign Up</button> 
                 
+       
             </form>
-        </div>
+       </div>
     )
 }
