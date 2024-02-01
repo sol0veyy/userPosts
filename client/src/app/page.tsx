7e68d50217674908a77935e83071205df89f30e6
sol0veyy <solovevdaniil03@mail.ru>
2024-02-01 20:47:01 +0300

    Add signup function for client

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,26 @@
+"use client"
+
+import { TokenPayload } from "@/http/user";
+import { jwtDecode } from "jwt-decode"
+
 export default function Home() {
-    return (
-        <div>
-            <h1>Main Page</h1>
-        </div>
-    );
+    const jwt_token = localStorage.getItem('jwt_token') || '';
+
+    if (jwt_token) {
+        const user: TokenPayload = jwtDecode(jwt_token)
+
+        return (
+            <div className="p-3">
+                <p>Login: {user.login}</p>
+                <p>Email: {user.email}</p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <p>Not Authorized</p>
+            </div>
+        );
+    }
 }
 
